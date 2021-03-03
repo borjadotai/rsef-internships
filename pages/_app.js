dd96c54e300082d23feae8eea264e65802ea432d
Borja Leiva <7519952+borjadotai@users.noreply.github.com>
2021-03-03 19:02:02 +0000

    Add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
+import {useEffect} from 'react'
 import Head from 'next/head'
+import {useRouter} from 'next/router'
 import {ChakraProvider} from '@chakra-ui/react'
+import * as gtag from '../utils/analytics/gtag'
 
 function MyApp({Component, pageProps}) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = url => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <>
       <Head>
